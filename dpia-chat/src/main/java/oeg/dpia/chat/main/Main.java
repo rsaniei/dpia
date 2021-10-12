8c4eaab08d5ccdf3d622edc452e1319c67ad4994
rsaniei <rsaniei@delicias.dia.fi.upm.es>
2021-10-12 11:43:57 +0200

    fix path

--- dpia-chat/src/main/java/oeg/dpia/chat/main/Main.java
+++ dpia-chat/src/main/java/oeg/dpia/chat/main/Main.java
@@ -5,19 +5,19 @@ import org.alicebot.ab.Chat;
 import org.alicebot.ab.configuration.BotConfiguration;
 
 /**
- * This main class demonstrates a simple example of using AIML.
- * AIML syntax: https://www.tutorialspoint.com/aiml/aiml_star_tag.htm 
+ * This main class demonstrates a simple example of using AIML. AIML syntax:
+ * https://www.tutorialspoint.com/aiml/aiml_star_tag.htm
+ *
  * @author vroddon
  */
 public class Main {
-    public static void main(String []args)
-    {
-        Bot alice = new Bot(BotConfiguration.builder().name("dpia").path("chat2\\src\\main\\resources").build());
+    public static void main(String[] args) {
+        Bot alice = new Bot(BotConfiguration.builder().name("dpia").path("chat2/src/main/resources").build());
         Chat chatSession = new Chat(alice);
         String answer = chatSession.multisentenceRespond("I like Beethoven");
         System.out.println(answer);
         answer = chatSession.multisentenceRespond("HELLO DPIA Assistant");
         System.out.println(answer);
     }
-    
+
 }
