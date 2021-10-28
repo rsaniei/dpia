ad511a27c426a4629a03a29784b3d15a8f3534a0
rsaniei <rsaniei@delicias.dia.fi.upm.es>
2021-10-28 22:57:59 +0200

    adding the aiml codes for dpia bot

--- dpia-chat/src/main/java/oeg/dpia/chat/main/Main.java
+++ dpia-chat/src/main/java/oeg/dpia/chat/main/Main.java
@@ -7,41 +7,41 @@ import org.alicebot.ab.Bot;
 import org.alicebot.ab.Chat;
 import org.alicebot.ab.MagicStrings;
 import org.alicebot.ab.PCAIMLProcessorExtension;
-import org.alicebot.ab.configuration.BotConfiguration;
+//import org.alicebot.ab.configuration.BotConfiguration;
 
 /**
- * This main class demonstrates a simple example of using AIML.
- * AIML syntax: https://www.tutorialspoint.com/aiml/aiml_star_tag.htm 
+ * This main class demonstrates a simple example of using AIML. AIML syntax:
+ * https://www.tutorialspoint.com/aiml/aiml_star_tag.htm
+ * 
  * @author vroddon
  */
 public class Main {
-    
-   
-    public static void main(String []args)
-    {
-        //The AIML file is in the src/main/resources/bots/aiml/dpia subfolder
+
+    public static void main(String[] args) {
+        // The AIML file is in the src/main/resources/bots/dpia/aiml subfolder
         MagicStrings.root_path = "src/main/resources";
         AIMLProcessor.extension = new PCAIMLProcessorExtension();
         Bot alice = new Bot("dpia");
         Chat chatSession = new Chat(alice);
-        System.out.println("Welcome to this chat.");
-        String answer="";
-        while(true)
-        {
-            try{
+        System.out.println("Welcome to this chat!");
+//        String answer = "";
+        while (true) {
+            try {
                 BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-                String input = reader.readLine();            
+                String input = reader.readLine();
                 if (input.equals("exit"))
                     break;
-                answer = chatSession.multisentenceRespond(input);
+                String answer = chatSession.multisentenceRespond(input);
+                System.out.println("Input is=>" + input);
+                System.out.println("Answer is=>" + answer);
+                
                 String question1 = chatSession.predicates.get("question1");
                 System.out.println(question1 + " - " + answer);
-            }catch(Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-        
+
     }
-    
+
 }
